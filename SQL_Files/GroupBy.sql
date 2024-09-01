45b6d288fd6bbd7230843e4c5ed184bfc60d9a63
WalterCSnet <93104153+WalterCSnet@users.noreply.github.com>
2024-09-01 21:14:06 +1000

    Adding having statement

--- SQL_Files/GroupBy.sql
+++ SQL_Files/GroupBy.sql
@@ -10,3 +10,58 @@ FROM table_name
 WHERE condition
 GROUP BY column_name(s)
 ORDER BY column_name(s);
+
+
+-- The following SQL statement lists the number of customers in each country, sorted high to low:
+
+Example
+SELECT COUNT(CustomerID), Country
+FROM Customers
+GROUP BY Country
+ORDER BY COUNT(CustomerID) DESC;
+
+
+GROUP BY With JOIN Example
+--- The following SQL statement lists the number of orders sent by each shipper:
+
+Example
+SELECT Shippers.ShipperName, COUNT(Orders.OrderID) AS NumberOfOrders FROM Orders
+LEFT JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID
+GROUP BY ShipperName;
+
+SQL HAVING Examples
+-- The following SQL statement lists the number of customers in each country. Only include countries with more than 5 customers:
+
+ExampleGet your own SQL Server
+SELECT COUNT(CustomerID), Country
+FROM Customers
+GROUP BY Country
+HAVING COUNT(CustomerID) > 5;
+
+-- Having
+
+-- The following SQL statement lists the number of customers in each country, sorted high to low (Only include countries with more than 5 customers):
+
+Example
+SELECT COUNT(CustomerID), Country
+FROM Customers
+GROUP BY Country
+HAVING COUNT(CustomerID) > 5
+ORDER BY COUNT(CustomerID) DESC;
+
+More HAVING Examples
+-- the following SQL statement lists the employees that have registered more than 10 orders:
+
+Example
+SELECT Employees.LastName, COUNT(Orders.OrderID) AS NumberOfOrders
+FROM (Orders
+INNER JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID)
+GROUP BY LastName
+HAVING COUNT(Orders.OrderID) > 10;
+
+SELECT Employees.LastName, COUNT(Orders.OrderID) AS NumberOfOrders
+FROM Orders
+INNER JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+WHERE LastName = 'Davolio' OR LastName = 'Fuller'
+GROUP BY LastName
+HAVING COUNT(Orders.OrderID) > 25;
